a4ae8b08edc30fbebfb6a0004a515876b4266569
Adeilson Á. A. Lima <92560985+adeilsonaalima@users.noreply.github.com>
2022-07-29 18:13:57 +0000

    revert: a97a780dd

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,6 @@ const root = createRoot(container);
 
 root.render(
   <StrictMode>
-    <Hapvida />
+    <Hapvida/>
   </StrictMode>
 );
--- src/pages/hapvida.jsx
+++ src/pages/hapvida.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from '../components/header';
 import Main from '../components/main';
-import Footer from '../components/footer';
 import '../styles/style.css';
 import '../styles/bulma.css';
 import '../styles/media-querys.css';
@@ -11,7 +10,6 @@ export default function Hapvida() {
 		<Fragment>
 			<Header />
 			<Main />
-			<Footer />
 		</Fragment>
 	);
 }
